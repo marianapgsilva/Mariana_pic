b259afb2b143d5670149a3c927e8f420261e345b
Mariana Silva <mariana.passarinho.g.silva.ms@gmail.com>
2026-06-02 22:17:24 +0100

    fix indentation error

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
  
- app.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
